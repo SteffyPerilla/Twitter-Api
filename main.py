5d4a80f0ded0ac9a6917396acdde485880d75ee2
Steffy Perilla <stefannyperilla@gmail.com>
2021-11-04 09:50:48 -0500

    feature(models_tweets)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Python
 from uuid import UUID
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 
 # Pydantic
@@ -39,11 +39,16 @@ class Users(UserBase):
     ) 
     birth_date: Optional[date] = Field(default=None)
 
-
-
-
 class Tweet(BaseModel):
-    pass
+    tweet_id: UUID = Field(...)
+    content: str = Field(
+        ...,
+        min_length=1,
+        max_length=256
+    )
+    create_at: datetime =Field(default=datetime.now())
+    update_at: Optional[datetime] =Field(default=None) 
+    by: Users = Field(...)
 
 
 
